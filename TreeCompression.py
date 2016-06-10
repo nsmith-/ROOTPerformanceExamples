7256472cbf6586470945b74a0b67c4a80e71a1a0
Nick Smith <ncsmith6@wisc.edu>
2016-06-10 10:40:51 +0200

    Example of lossy compression
    Reducing significant figures in float saves space

--- TreeCompression.py
+++ TreeCompression.py
@@ -1,5 +1,6 @@
 import ROOT
 import array
+import struct
 
 file = ROOT.TFile('treecompression.root', 'recreate')
 
@@ -8,11 +9,21 @@ tree = ROOT.TTree('treeCompression', 'test compression')
 boolVal = array.array('f', [0.])
 branch = tree.Branch('boolBranch', boolVal, 'boolBranch/f')
 
-for i in range(1000000) :
+randVal = array.array('f', [0.])
+randB = tree.Branch('randVal', randVal, 'randVal/f')
+
+randValLossy = array.array('f', [0.])
+randBLossy = tree.Branch('randValLossy', randValLossy, 'randValLossy/f')
+
+toint = lambda f: struct.unpack('i', struct.pack('f', f))[0]
+tofloat = lambda i: struct.unpack('f', struct.pack('i', i))[0]
+lossy = lambda x: tofloat(toint(x) & ~(2**10-1))
+
+for i in range(4000000) :
+    randVal[0] = ROOT.gRandom.Gaus()
+    randValLossy[0] = lossy(randVal[0])
     tree.Fill()
 
 tree.Write()
 
 tree.Print()
-
-file.Close()
